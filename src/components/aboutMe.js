cf4edd600b9b8b4f7345becc0953632474077725
Mari Zaq <m.zaqareishvili99@gmail.com>
2022-03-20 21:04:04 +0400

    check

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -13,7 +13,7 @@ function AboutMe() {
           letters, as opposed to using 'Content here, content here', making it
           look like readable English. Many desktop publishing packages and web
           page editors now use Lorem Ipsum as their default model text, and a
-          search for 'lorem ipsum' will uncover many web sites still in their
+          search for 'lorem im' will uncover many web sites still in their
           infancy. Various versions have evolved over the years, sometimes by
           accident, sometimes on purpose (injected humour and the like).
         </div>
