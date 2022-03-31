4b3c980ea73bf0846d12cd6ce0739c2c5ab648dc
Mari Zaq <m.zaqareishvili99@gmail.com>
2022-03-31 23:43:42 +0400

    deploy on github

--- src/index.js
+++ src/index.js
@@ -2,25 +2,40 @@ import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+
 import reportWebVitals from "./reportWebVitals";
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import HttpApi from "i18next-http-backend";
 
+import translationEn from "./locales/en/translation.json";
+import translationKa from "./locales/ka/translation.json";
+
+const resources = {
+  en: {
+    translation: translationEn,
+  },
+  ka: {
+    translation: translationKa,
+  },
+};
+
 i18n
   .use(initReactI18next)
   .use(LanguageDetector)
   .use(HttpApi)
   .init({
+    resources,
     supportedLngs: ["en", "ka"],
     fallbackLng: "en",
+    keySeparator: false,
     detection: {
       order: ["cookie", "htmlTag", "localStorage", "path", "subdomain"],
       caches: ["cookie"],
     },
-    backend: {
-      loadPath: "/assets/locales/{{lng}}/translation.json",
+    interpolation: {
+      escapeValue: false,
     },
   });
 
